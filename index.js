440cc50fdd830f70435ded35700276d75136b994
Tushar Borole <tushariscoolster@gmail.com>
2017-10-10 20:48:10 -0700

    Demo for the file is added

---
+++ index.js
@@ -0,0 +1,47 @@
+
+var jsoncss = require("jsoncss");
+var fs= require('fs');
+var _ = require('lodash');
+function CssHelperPlugin(options) {
+    // Configure your plugin with options...
+    this.options = _.extend({config:'css.config.json'}, options);
+
+}
+
+CssHelperPlugin.prototype.apply = function(compiler) {
+    compiler.plugin("compile", function(params) {
+        console.log(params);
+    });
+
+    compiler.plugin("compilation", function(compilation) {
+        console.log("The compiler is starting a new compilation...");
+
+        compilation.plugin("optimize", function() {
+            console.log("The compilation is starting to optimize files...");
+        });
+    });
+
+    compiler.plugin("emit", function(compilation, callback) {
+        console.log("The compilation is going to emit files...");
+
+        jsoncss.convert({
+            "body": {
+                "color":"red"
+            }
+        }, '', function(err, result) {
+            if (err) {
+                return console.log(err);
+            }
+            fs.writeFile('csshelper.css', result, function(err){
+                if (err) {
+                    return console.log(err);
+                }
+                callback();
+            });
+
+        });
+
+    });
+};
+
+module.exports = CssHelperPlugin;
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var CssHelper = require('./index.js');
+
+module.exports = {
+    // ... config settings here ...
+    plugins: [
+        new CssHelper({config:'css.config.json'})
+    ],
+    entry: './app.js',
+    output: {
+        filename: 'bundle.js'
+    }
+};
